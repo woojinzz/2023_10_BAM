e9901444226aebdfa774b7de288575b727c4d92b
dnwls3984 <dnwls3984@gmail.com>
2023-10-04 16:57:10 +0900

    git 업로드 연습

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println("=시작=");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String cmd = sc.nextLine();
+		int cmd2 = sc.nextInt();
+		sc.next();
+		
+		System.out.println(cmd);
+		System.out.println(cmd2);
+		
+		System.out.println("=끝=");
+		
+	}
+}
